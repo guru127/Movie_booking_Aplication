9c5cb38356ef62c3599fd6b51badb6949e3d0d3c
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-22 12:23:55 +0530

    adding starter pom and checking if spring boot create bean for it

--- src/main/java/com/upgrad/mba/MovieBookingApplication.java
+++ src/main/java/com/upgrad/mba/MovieBookingApplication.java
@@ -2,12 +2,16 @@ package com.upgrad.mba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class MovieBookingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieBookingApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MovieBookingApplication.class, args);
+		DataSource dataSource = context.getBean(DataSource.class);
 	}
 
 }
