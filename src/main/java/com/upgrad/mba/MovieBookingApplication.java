101eeee032a53b250b85054aa77c956bd71b8fea
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-22 13:30:19 +0530

    cleaning movie booking application

--- src/main/java/com/upgrad/mba/MovieBookingApplication.java
+++ src/main/java/com/upgrad/mba/MovieBookingApplication.java
@@ -2,34 +2,12 @@ package com.upgrad.mba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-
-import javax.sql.DataSource;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class MovieBookingApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(MovieBookingApplication.class, args);
-		DataSource dataSource = context.getBean(DataSource.class);
-
-		System.out.println("**********************************");
-		Resource resourceDev = new ClassPathResource("application-dev.properties");
-		System.out.println(resourceDev.exists());
-		System.out.println("**********************************");
-
-		System.out.println("**********************************");
-		System.out.println(context.getBeanDefinitionCount());
-		System.out.println(
-				Arrays.stream(context.getBeanDefinitionNames())
-						.collect(Collectors.toList())
-						.contains("myService")
-		);
-		System.out.println("**********************************");
+		SpringApplication.run(MovieBookingApplication.class, args);
 	}
 
 }
--- src/main/java/com/upgrad/mba/MyService.java
+++
@@ -1,9 +0,0 @@
-package com.upgrad.mba;
-
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
-@Component
-@Profile("dev")
-public class MyService {
-}
