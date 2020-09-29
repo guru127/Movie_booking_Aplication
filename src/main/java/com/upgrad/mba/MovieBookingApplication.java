599477e64116709c13a4a6ab1b0b115b3447ad10
Ishwar Soni <ishwar.soni@upgrad.com>
2020-09-29 09:52:50 +0530

    TheatreDao using Spring Data JPA

--- src/main/java/com/upgrad/mba/MovieBookingApplication.java
+++ src/main/java/com/upgrad/mba/MovieBookingApplication.java
@@ -3,9 +3,11 @@ package com.upgrad.mba;
 import com.upgrad.mba.dao.CityDao;
 import com.upgrad.mba.dao.CustomerDao;
 import com.upgrad.mba.dao.MovieDao;
+import com.upgrad.mba.dao.TheatreDao;
 import com.upgrad.mba.entities.City;
 import com.upgrad.mba.entities.Customer;
 import com.upgrad.mba.entities.Movie;
+import com.upgrad.mba.entities.Theatre;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -21,73 +23,44 @@ public class MovieBookingApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(MovieBookingApplication.class, args);
-		MovieDao movieDao = context.getBean(MovieDao.class);
+		TheatreDao theatreDao = context.getBean(TheatreDao.class);
 
-		Movie movie1 = new Movie();
-		movie1.setMovieName("Avengers: Infinity War");
-		movie1.setMovieDescription("The Avengers must stop Thanos, an intergalactic warlord, " +
-				"from getting his hands on all the infinity stones.");
-		movie1.setReleaseDate(LocalDateTime.of(2018, 4, 27, 5, 30));
-		movie1.setDuration(150);
-		movie1.setCoverPhotoUrl("cover-photo-url");
-		movie1.setTrailerUrl("trailer-url");
+		Theatre theatre1 = new Theatre();
+		theatre1.setTheatreName("Urvashi Cinema");
+		theatre1.setTicketPrice(500);
 
-		Movie movie2 = new Movie();
-		movie2.setMovieName("Avengers: Endgame");
-		movie2.setMovieDescription("After Thanos, an intergalactic warlord, disintegrates half of " +
-				"the universe, the Avengers must reunite and assemble again to reinvigorate their " +
-				"trounced allies and restore balance.");
-		movie2.setReleaseDate(LocalDateTime.of(2019, 4, 26, 5, 30));
-		movie2.setDuration(180);
-		movie2.setCoverPhotoUrl("cover-photo-url");
-		movie2.setTrailerUrl("trailer-url");
+		Theatre theatre2 = new Theatre();
+		theatre2.setTheatreName("Cinepolis Multiplex");
+		theatre2.setTicketPrice(600);
 
-		Movie movie3 = new Movie();
-		movie3.setMovieName("Black Panther");
-		movie3.setMovieDescription("After his father's death, T'Challa returns home to Wakanda to " +
-				"inherit his throne. However, a powerful enemy related to his family threatens to " +
-				"attack his nation.");
-		movie3.setReleaseDate(LocalDateTime.of(2018, 1, 29, 5, 30));
-		movie3.setDuration(150);
-		movie3.setCoverPhotoUrl("cover-photo-url");
-		movie3.setTrailerUrl("trailer-url");
+		Theatre theatre3 = new Theatre();
+		theatre3.setTheatreName("PVR Koramangla");
+		theatre3.setTicketPrice(550);
 
-		Movie movie4 = new Movie();
-		movie4.setMovieName("Spider-Man: Into the Spider-Verse");
-		movie4.setMovieDescription("After gaining superpowers from a spider bite, Miles Morales protects" +
-				" the city as Spider-Man. Soon, he meets alternate versions of himself and gets embroiled" +
-				" in an epic battle to save the multiverse.");
-		movie4.setReleaseDate(LocalDateTime.of(2018, 12, 14, 5, 30));
-		movie4.setDuration(120);
-		movie4.setCoverPhotoUrl("cover-photo-url");
-		movie4.setTrailerUrl("trailer-url");
+		Theatre theatre4 = new Theatre();
+		theatre4.setTheatreName("PVR IMAX");
+		theatre4.setTicketPrice(700);
 
-		Movie movie5 = new Movie();
-		movie5.setMovieName("Deadpool 2");
-		movie5.setMovieDescription("Deadpool protects a young mutant Russell from the authorities and gets" +
-				" thrown in prison. However, he escapes and forms a team of mutants to prevent a time-travelling" +
-				" mercenary from killing Russell.");
-		movie5.setReleaseDate(LocalDateTime.of(2018, 5, 1, 5, 30));
-		movie5.setDuration(150);
-		movie5.setCoverPhotoUrl("cover-photo-url");
-		movie5.setTrailerUrl("trailer-url");
+		Theatre theatre5 = new Theatre();
+		theatre5.setTheatreName("INOX");
+		theatre5.setTicketPrice(400);
 
-		movieDao.saveAll(List.of(movie1, movie2, movie3, movie4, movie5));
+		theatreDao.saveAll(List.of(theatre1, theatre2, theatre3, theatre4, theatre5));
 
-		System.out.println("*************Finding all movies*************");
-		movieDao.findAll().forEach(movie -> System.out.println(movie.getMovieName()));
+		System.out.println("*************Finding all theatres*************");
+		theatreDao.findAll().forEach(theatre -> System.out.println(theatre.getTheatreName()));
 
-		System.out.println("*************Finding first page of movies*************");
-		Page<Movie> page0 = movieDao.findAll(PageRequest.of(0, 2));
-		page0.stream().forEach(movie -> System.out.println(movie.getMovieName()));
+		System.out.println("*************Finding first page of theatres*************");
+		Page<Theatre> page0 = theatreDao.findAll(PageRequest.of(0, 2));
+		page0.stream().forEach(theatre -> System.out.println(theatre.getTheatreName()));
 
-		System.out.println("*************Finding second page of movies*************");
-		Page<Movie> page1 = movieDao.findAll(PageRequest.of(1, 2));
-		page1.stream().forEach(movie -> System.out.println(movie.getMovieName()));
+		System.out.println("*************Finding second page of theatres*************");
+		Page<Theatre> page1 = theatreDao.findAll(PageRequest.of(1, 2));
+		page1.stream().forEach(theatre -> System.out.println(theatre.getTheatreName()));
 
-		System.out.println("*************Finding first page of movies based on ascending duration*************");
-		Page<Movie> page0sorted = movieDao.findAll(PageRequest.of(0, 2, Sort.by("duration").ascending()));
-		page0sorted.stream().forEach(movie -> System.out.println(movie.getMovieName()));
+		System.out.println("*************Finding first page of theaters based on ascending price*************");
+		Page<Theatre> page0sorted = theatreDao.findAll(PageRequest.of(0, 2, Sort.by("ticketPrice").ascending()));
+		page0sorted.stream().forEach(theatre -> System.out.println(theatre.getTheatreName()));
 	}
 
 }
