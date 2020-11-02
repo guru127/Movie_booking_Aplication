d8a4ba0a91f212db053ab0956a1d29de37bc2a7b
Ashita Jain <jain.ashita@upgrad.com>
2020-11-02 13:58:58 +0530

    added GET endpoint to return object

--- src/main/java/com/upgrad/mba/controllers/MovieController.java
+++ src/main/java/com/upgrad/mba/controllers/MovieController.java
@@ -1,13 +1,31 @@
 package com.upgrad.mba.controllers;
 
+import com.upgrad.mba.dto.MovieDTO;
+import com.upgrad.mba.entities.Movie;
+import com.upgrad.mba.exceptions.MovieDetailsNotFoundException;
+import com.upgrad.mba.services.MovieService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieController {
+    @Autowired
+    MovieService movieService;
+
+    @Autowired
+    ModelMapper modelmapper;
 
     @GetMapping(value= {"/sayHelloMovie"})
     public String sayHello(){
         return "Hello World To All From MovieController";
+    }
+
+    @GetMapping(value= {"/movie/9"})
+    public MovieDTO getMovieDetailsofId() throws MovieDetailsNotFoundException {
+        Movie responseMovie = movieService.getMovieDetails(9);
+        MovieDTO responseMovieDTO = modelmapper.map(responseMovie,MovieDTO.class);
+        return(responseMovieDTO);
     }
 }
