375c3e88152afc7b3c8f0d46d8a7f7d16ac15cee
Ashita Jain <jain.ashita@upgrad.com>
2020-11-02 13:42:24 +0530

    ModelMapper setup

--- src/main/java/com/upgrad/mba/MovieBookingApplication.java
+++ src/main/java/com/upgrad/mba/MovieBookingApplication.java
@@ -6,9 +6,11 @@ import com.upgrad.mba.services.CityService;
 import com.upgrad.mba.services.MovieService;
 import com.upgrad.mba.services.StatusService;
 import com.upgrad.mba.services.TheatreService;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -103,6 +105,11 @@ public class MovieBookingApplication {
 //		movieService.getAllMoviesDetails().forEach(System.out::println);
 //
 //		theatreService.getAllTheatreDetails().forEach(System.out::println);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
