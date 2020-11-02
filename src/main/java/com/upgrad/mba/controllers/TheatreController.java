c69947c1ead0898ea8003a0d3acc332a38f081b0
Ashita Jain <jain.ashita@upgrad.com>
2020-11-02 14:04:13 +0530

    added GET endpoint to return TheatreDetails using Id

---
+++ src/main/java/com/upgrad/mba/controllers/TheatreController.java
@@ -0,0 +1,29 @@
+package com.upgrad.mba.controllers;
+
+import com.upgrad.mba.dto.TheatreDTO;
+import com.upgrad.mba.entities.Theatre;
+import com.upgrad.mba.exceptions.TheatreDetailsNotFoundException;
+import com.upgrad.mba.services.TheatreService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TheatreController {
+    @Autowired
+    TheatreService theatreService;
+
+    @Autowired
+    ModelMapper modelmapper;
+
+    @GetMapping(value = "/theatres/{id}")
+    public ResponseEntity getTheatreDetails(@PathVariable(name = "id") int id) throws TheatreDetailsNotFoundException {
+        Theatre responseTheatre = theatreService.getTheatreDetails(id);
+        TheatreDTO responseTheatreDTO = modelmapper.map(responseTheatre,TheatreDTO.class);
+        return new ResponseEntity<>(responseTheatreDTO, HttpStatus.OK);
+    }
+}
