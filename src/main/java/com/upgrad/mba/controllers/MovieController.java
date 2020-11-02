2c437b3f345371e12f4aae9bf33b3dcbae8d08a6
Ashita Jain <jain.ashita@upgrad.com>
2020-11-02 13:51:29 +0530

    added MovieController

--- src/main/java/com/upgrad/mba/controllers/MovieController.java
+++ src/main/java/com/upgrad/mba/controllers/MovieController.java
@@ -1,2 +1,13 @@
-package com.upgrad.mba.controllers;public class MovieController {
+package com.upgrad.mba.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieController {
+
+    @GetMapping(value= {"/sayHelloMovie"})
+    public String sayHello(){
+        return "Hello World To All From MovieController";
+    }
 }
