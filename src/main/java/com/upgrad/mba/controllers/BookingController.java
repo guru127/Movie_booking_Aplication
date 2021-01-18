20e1bbf75468e0295a3a9bf53529f684eb30a981
Ashita Jain <Ashita.Jain@upgrad.local>
2021-01-18 18:50:35 +0530

    add new Booking API

--- src/main/java/com/upgrad/mba/controllers/BookingController.java
+++ src/main/java/com/upgrad/mba/controllers/BookingController.java
@@ -3,16 +3,17 @@ package com.upgrad.mba.controllers;
 import com.upgrad.mba.dto.BookingDTO;
 import com.upgrad.mba.entities.Booking;
 import com.upgrad.mba.exceptions.BookingDetailsNotFoundException;
+import com.upgrad.mba.exceptions.CustomerDetailsNotFoundException;
+import com.upgrad.mba.exceptions.MovieTheatreDetailsNotFoundException;
 import com.upgrad.mba.services.BookingService;
+import com.upgrad.mba.utils.DTOEntityConverter;
+import com.upgrad.mba.utils.EntityDTOConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +26,12 @@ public class BookingController {
     @Autowired
     ModelMapper modelmapper;
 
+    @Autowired
+    EntityDTOConverter entityDTOConverter;
+
+    @Autowired
+    DTOEntityConverter dtoEntityConverter;
+
     @GetMapping(value = "/bookings/{id}")
     public ResponseEntity getBookingDetails(@PathVariable(name = "id") int id) throws BookingDetailsNotFoundException {
         Booking responseBooking = bookingService.getBookingDetails(id);
@@ -36,5 +43,14 @@ public class BookingController {
     public ResponseEntity findAllBookings() {
         List<Booking> bookings = bookingService.getAllBookingDetails();
         return new ResponseEntity<>(bookings, HttpStatus.OK);
+    }
+
+    @PostMapping(value="/bookings",consumes= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity newBooking(@RequestBody BookingDTO bookingDTO) throws CustomerDetailsNotFoundException, MovieTheatreDetailsNotFoundException {
+        Booking newBooking = dtoEntityConverter.convertToBookingEntity(bookingDTO);
+        Booking savedBooking = bookingService.acceptBookingDetails(newBooking);
+        BookingDTO savedBookingDTO = entityDTOConverter.convertToBookingDTO(savedBooking);
+        savedBookingDTO.setMovieTheatreId(savedBooking.getMovieTheatre().getMovieTheatreId());
+        return new ResponseEntity<>(savedBookingDTO, HttpStatus.CREATED);
     }
 }
