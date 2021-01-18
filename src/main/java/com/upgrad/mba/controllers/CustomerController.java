382909b00415ed1a98ada35db699cbe5922a5747
Ashita Jain <Ashita.Jain@upgrad.local>
2021-01-18 18:34:08 +0530

    update Customer Detail API and add CustomerDTO

---
+++ src/main/java/com/upgrad/mba/controllers/CustomerController.java
@@ -0,0 +1,30 @@
+package com.upgrad.mba.controllers;
+
+import com.upgrad.mba.dto.CustomerDTO;
+import com.upgrad.mba.entities.Customer;
+import com.upgrad.mba.exceptions.*;
+import com.upgrad.mba.services.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value="/movie_app/v1")
+public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
+    @Autowired
+    ModelMapper modelmapper;
+
+    @PutMapping(value="/customers/{id}",consumes= MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity updateCustomerDetails(@PathVariable(name = "id") int id , @RequestBody CustomerDTO customerDTO) throws CustomerUserNameExistsException, CustomerDetailsNotFoundException, UserTypeDetailsNotFoundException, TheatreDetailsNotFoundException, BookingDetailsNotFoundException {
+        Customer newCustomer = modelmapper.map(customerDTO,Customer.class);
+        Customer updatedCustomer =  customerService.updateCustomerDetails(id, newCustomer);
+        CustomerDTO updatedCustomerDTO = modelmapper.map(updatedCustomer, CustomerDTO.class);
+        return new ResponseEntity<>(updatedCustomerDTO, HttpStatus.OK);
+    }
+}
