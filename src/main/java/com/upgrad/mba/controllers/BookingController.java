ec2fed9d275fd0140675d68e73c715e206ae7ffd
Ashita Jain <Ashita.Jain@upgrad.local>
2021-01-18 18:23:45 +0530

    added request path

--- src/main/java/com/upgrad/mba/controllers/BookingController.java
+++ src/main/java/com/upgrad/mba/controllers/BookingController.java
@@ -10,9 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/movie_app/v1")
 public class BookingController {
     @Autowired
     BookingService bookingService;
