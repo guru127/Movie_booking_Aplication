5bf376546dae92dd579a92ef6cf708c930302f82
Ashita Jain <Ashita.Jain@upgrad.local>
2021-01-18 18:29:58 +0530

    add new Theatre API

--- src/main/java/com/upgrad/mba/controllers/TheatreController.java
+++ src/main/java/com/upgrad/mba/controllers/TheatreController.java
@@ -7,11 +7,9 @@ import com.upgrad.mba.services.TheatreService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="/movie_app/v1")
@@ -27,5 +25,13 @@ public class TheatreController {
         Theatre responseTheatre = theatreService.getTheatreDetails(id);
         TheatreDTO responseTheatreDTO = modelmapper.map(responseTheatre,TheatreDTO.class);
         return new ResponseEntity<>(responseTheatreDTO, HttpStatus.OK);
+    }
+
+    @PostMapping(value="/theatres",consumes= MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity newTheatre(@RequestBody TheatreDTO theatreDTO) {
+        Theatre newTheatre = modelmapper.map(theatreDTO, Theatre.class);
+        Theatre savedTheatre = theatreService.acceptTheatreDetails(newTheatre);
+        TheatreDTO savedTheatreDTO = modelmapper.map(savedTheatre, TheatreDTO.class);
+        return new ResponseEntity<>(savedTheatreDTO, HttpStatus.CREATED);
     }
 }
