1718d7b84e2c386dcc8ca3877d67ea0567648469
Ashita Jain <Ashita.Jain@upgrad.local>
2021-03-08 14:13:13 +0530

    Modified User Type to Customer

--- src/main/java/com/upgrad/mba/entities/UserType.java
+++ src/main/java/com/upgrad/mba/entities/UserType.java
@@ -15,7 +15,7 @@ public class UserType {
     private int userTypeId;
 
     @Column(length = 20, unique = true)
-    private String userTypeName = "User";
+    private String userTypeName = "Customer";
 
     public UserType() {
     }
